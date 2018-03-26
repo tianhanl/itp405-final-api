672dd3bc7bf35064c0ac8f33340d5f4af9c70403
Tianhang Liu <tianhanl@usc.edu>
2018-03-25 21:23:38 -0700

    use dotenv to separate credentials

--- app.js
+++ app.js
@@ -11,80 +11,81 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 
-app.get('/categories', (req, res) => {
-    database
-        .getCategories()
-        .then(rows => {
-            const output = {
-                categories: rows
-            };
-            res.json(output);
-        })
-        .catch(err => {
-            console.log(err);
-            next();
-        });
+app.get('/categories', (req, res, next) => {
+  database
+    .getCategories()
+    .then(rows => {
+      const output = {
+        categories: rows
+      };
+      res.json(output);
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
 });
 
-app.get('/items', (req, res) => {
-    database
-        .getItems(req.query.page)
-        .then(rows => {
-            const output = {
-                items: rows,
-                page: 1
-            };
-            res.json(output);
-        })
-        .catch(err => {
-            console.log(err);
-            next();
-        });
+app.get('/items', (req, res, next) => {
+  database
+    .getItems(req.query.page)
+    .then(rows => {
+      const output = {
+        items: rows,
+        page: 1
+      };
+      res.json(output);
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
 });
 
-app.get('/transactions', (req, res) => {
-    database
-        .getTransactions(req.query.page)
-        .then(rows => {
-            const output = {
-                transactions: rows,
-                page: 1
-            };
-            res.json(output);
-        })
-        .catch(err => {
-            console.log(err);
-            next();
-        });
+app.get('/transactions', (req, res, next) => {
+  database
+    .getTransactions(req.query.page)
+    .then(rows => {
+      const output = {
+        transactions: rows,
+        page: 1
+      };
+      res.json(output);
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
 });
-
-app.post('/transactions', (req, res) => {
-    const userId = req.body.userid;
-    const itemId = req.body.itemid;
-    const date = req.body.date;
-    const quantity = req.body.quantity;
-    const total = req.body.total;
-    database
-        .postTransaction(userId, itemId, date, quantity, total)
-        .then(rows => {
-            res.status(202);
-            res.json(rows);
-        })
-        .catch(err => {
-            res.status(422);
-            res.send(err);
-        });
+app.post('/transactions', (req, res, next) => {
+  console.log(req.body);
+  const userId = Number.parseInt(req.body.userId);
+  const itemId = Number.parseInt(req.body.itemId);
+  const date = req.body.date;
+  const quantity = Number.parseInt(req.body.quantity);
+  const total = Number.parseInt(req.body.total);
+  database
+    .postTransaction(userId, itemId, date, quantity, total)
+    .then(rows => {
+      res.status(202);
+      res.json(rows);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(422);
+      res.send(err);
+    });
 });
 
 
 
 // catch all requests for not existed routes
 app.use((req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Resource Not Found');
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Resource Not Found');
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Server Started');
+  console.log('Server Started');
 });
--- util/database.js
+++ util/database.js
@@ -1,70 +1,72 @@
-const credential = require('./credential');
-
+require('dotenv').config();
 const knex = require('knex');
 
 const rowLimit = 12;
 
 const connect = () => knex({
-    client: 'mysql',
-    connection: {
-        host: credential.host,
-        user: credential.user,
-        password: credential.pass,
-        database: credential.db,
-    }
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  }
 });
 
+
 const getItems = (page = 1) => {
-    const offsetNumber = (page - 1) * rowLimit;
-    return connect()('items')
-        .join('categories', 'items.category_id', '=', 'categories.id')
-        .limit(rowLimit)
-        .offset(offsetNumber)
-        .select('items.id', 'items.name', 'price', 'img_url', 'description', 'release_date', 'categories.name as category_name');
+  const offsetNumber = (page - 1) * rowLimit;
+  return connect()('items')
+    .join('categories', 'items.category_id', '=', 'categories.id')
+    .limit(rowLimit)
+    .offset(offsetNumber)
+    .select('items.id', 'items.name', 'price', 'img_url', 'description', 'release_date', 'categories.name as category_name');
 };
 
 const getCategories = () => {
-    return connect()('categories');
+  return connect()('categories');
 };
 
 const getTransactions = (page = 1) => {
-    const offsetNumber = (page - 1) * rowLimit;
-    return connect()('transactions')
-        .join('users', 'transactions.user_id', '=', 'users.id')
-        .join('items', 'transactions.item_id', '=', 'items.id')
-        .limit(rowLimit)
-        .offset(offsetNumber)
-        .select('transactions.id', 'users.username', 'items.name as item_name', 'transactions.date', 'transactions.quantity', 'transactions.total');
+  const offsetNumber = (page - 1) * rowLimit;
+  return connect()('transactions')
+    .join('users', 'transactions.user_id', '=', 'users.id')
+    .join('items', 'transactions.item_id', '=', 'items.id')
+    .limit(rowLimit)
+    .offset(offsetNumber)
+    .select('transactions.id', 'users.username', 'items.name as item_name', 'transactions.date', 'transactions.quantity', 'transactions.total');
 };
 
 const getTransactionsForUser = (user_id, page = 1) => {
-    const offsetNumber = (page - 1) * rowLimit;
-    return connect()('transactions')
-        .join('users', 'transactions.user_id', '=', 'users.id')
-        .join('items', 'transactions.item_id', '=', 'items.id')
-        .limit(rowLimit)
-        .offset(offsetNumber)
-        .where({ user_id })
-        .select('transactions.id', 'users.username', 'items.name as item_name', 'transactions.date', 'transactions.quantity', 'transactions.total')
+  const offsetNumber = (page - 1) * rowLimit;
+  return connect()('transactions')
+    .join('users', 'transactions.user_id', '=', 'users.id')
+    .join('items', 'transactions.item_id', '=', 'items.id')
+    .limit(rowLimit)
+    .offset(offsetNumber)
+    .where({
+      user_id
+    })
+    .select('transactions.id', 'users.username', 'items.name as item_name', 'transactions.date', 'transactions.quantity', 'transactions.total')
 };
 
 const postTransaction = (userId, itemId, date, quantity, total) => {
-    return connect()
-        .insert({
-            user_id: userId,
-            item_id: itemId,
-            date,
-            quantity,
-            total
-        })
-        .into('transactions');
+  return connect()
+    .insert({
+      user_id: userId,
+      item_id: itemId,
+      date: date,
+      quantity: quantity,
+      total
+    })
+    .into('transactions');
 };
 
 module.exports = {
-    connect,
-    getItems,
-    getCategories,
-    getTransactions,
-    getTransactionsForUser,
-    postTransaction
+  connect,
+  getItems,
+  getCategories,
+  getTransactions,
+  getTransactionsForUser,
+  postTransaction
 };
